ea00065fc5c6e118e6c325478b11cda7b5b80950
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-07 16:36:22 +0200

    [UPD] Stilizzazioni tramite css e pulizia del codice

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,36 @@
 // recupero gli elementi dal DOM
+const card = document.querySelector('.card');
+const cardContainer = document.querySelector('.card-container');
 
-const card = document.querySelector('.card')
-const date = document.querySelector('.text-data')
-const cardContainer = document.querySelector('.card-container')
+
 // creo una funzione che generi delle card con un'immagine e del testo sotto
 // dichiaro la funzione
-
-
-//creo un ciclo for per generare nuove le card
-
 const generatoreCard = () => {
 
+    //creo un ciclo for per generare ciascuna card
     for (let i = 0; i < 6; i++) {
+        // effettuo una chiamata ajax all'API 
         axios.get('https://lanciweb.github.io/demo/api/pictures/').then(element => {
+            //recupero le propietà che mi servono per generare le cards
             dateGenerate = element.data[i].date;
             titleGenerate = element.data[i].title;
             imageGenerate = element.data[i].url;
+            //creo l'elemento del DOM
             const card = `
                 <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
                     <div><img class="ping" src="./img/pin.svg" alt="ping"></div>
                      <div class="card">
-                        <img src="${imageGenerate}" class="card-img-top my-padding" alt="...">
+                        <img src="${imageGenerate}" class="card-img-top my-padding" alt="image card">
                         <div class="card-body">
-                            <data class="card-title text-data">${dateGenerate}</data>
-                            <p class="card-text">${titleGenerate}</p>
+                            <p class="card-title text-data my-data">${dateGenerate}</p>
+                            <p class="card-text my-text">${titleGenerate.toUpperCase()}</p>
                         </div>
                     </div>
                 </div>`;
+            //inserisco l'elemento nel DOM
             cardContainer.innerHTML += card;
         })
     }
 }
-
-generatoreCard()
+// richiamo la funzione
+generatoreCard();
