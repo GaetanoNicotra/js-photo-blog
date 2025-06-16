740505d89d398fc4ccb94d88f7b6440b7df1e336
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-06-16 19:35:29 +0200

    [UPD] implementato overlay per visualizzazione immagini e migliorato il layout

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,18 @@
 // recupero gli elementi dal DOM
 const card = document.querySelector('.card');
 const cardContainer = document.querySelector('.card-container');
-
+const overlayContainer = document.querySelector('.overlay')
 
 // creo una funzione che generi delle card con un'immagine e del testo sotto
 // dichiaro la funzione
 const generatoreCard = () => {
 
-    //creo un ciclo for per generare ciascuna card
-    for (let i = 0; i < 6; i++) {
-        // effettuo una chiamata ajax all'API 
-        axios.get('https://lanciweb.github.io/demo/api/pictures/').then(element => {
+    // effettuo una chiamata ajax all'API 
+    axios.get('https://lanciweb.github.io/demo/api/pictures/').then(element => {
+
+        //creo un ciclo for per generare ciascuna card
+        for (let i = 0; i < 6; i++) {
+
             //recupero le propietà che mi servono per generare le cards
             let dateGenerate = element.data[i].date;
             let titleGenerate = element.data[i].title;
@@ -20,7 +22,7 @@ const generatoreCard = () => {
                 <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
                     <div class="ping"><img src="./img/pin.svg" alt="ping"></div>
                      <div class="card my-width">
-                        <img src="${imageGenerate}" class="card-img-top my-padding " alt="image card">
+                        <img src="${imageGenerate}" class="card-img-top my-padding overlay-trigger " alt="image card">
                         <div class="card-body">
                             <p class="card-title text-data my-data">${dateGenerate}</p>
                             <p class="card-text my-text">${titleGenerate.toUpperCase()}</p>
@@ -30,20 +32,20 @@ const generatoreCard = () => {
             //inserisco l'elemento nel DOM
             cardContainer.innerHTML += card;
 
-
-            // creo l'effetto di overlay che dovrà
-            // verificarsi al click sulla foto
-            const imageOverlay = document.getElementById('image-overlay');
-            const imageClick = cardContainer.querySelectorAll('img')
-            imageClick.forEach(image => {
-                image.addEventListener('click', function () {
+            // creo l'overlay per l'immagine
+            const images = cardContainer.querySelectorAll('.overlay-trigger');
+            images.forEach(img => {
+                img.addEventListener('click', () => {
+                    imageOverlay.setAttribute('src', img.src);
+                    overlayContainer.classList.remove('d-none');
                     imageOverlay.classList.remove('d-none');
                     closeButton.classList.remove('d-none');
                 });
             })
-        })
-    }
+        }
+    })
 }
+
 // richiamo la funzione
 generatoreCard();
 
@@ -52,6 +54,7 @@ const imageOverlay = document.getElementById('image-overlay');
 const closeButton = document.getElementById('close');
 closeButton.addEventListener('click', function () {
     imageOverlay.classList.add('d-none');
+    overlayContainer.classList.add('d-none');
     closeButton.classList.add('d-none');
 })
 
